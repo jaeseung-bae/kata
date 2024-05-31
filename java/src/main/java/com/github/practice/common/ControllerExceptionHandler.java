acb15267f67a7ffbfe68dd60d30414dc4b5d3691
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-05-31 11:11:53 +0900

    feat: introduce ControllerExceptionHandler

---
+++ java/src/main/java/com/github/practice/common/ControllerExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.github.practice.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(basePackages = "com.github.practice.controller" )
+public class ControllerExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
