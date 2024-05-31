d84e6d7b8a6be1c67f040c0e9cd0e69c95dff622
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-05-31 11:11:01 +0900

    feat: introduce service and delegate logic to service

--- java/src/main/java/com/github/practice/controller/ArticleApiController.java
+++ java/src/main/java/com/github/practice/controller/ArticleApiController.java
@@ -10,60 +10,51 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.github.practice.domain.Article;
 import com.github.practice.dto.ArticleFormDTO;
 import com.github.practice.repository.ArticleRepository;
+import com.github.practice.service.ArticleService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+//@RestControllerAdvice
+@AllArgsConstructor
 public class ArticleApiController {
     private final ArticleRepository repository;
-
-    public ArticleApiController(ArticleRepository repository) {this.repository = repository;}
+    private final ArticleService service;
 
     @PostMapping("/api/articles")
     public Article create(@RequestBody ArticleFormDTO dto) {
-        Article saved = repository.save(dto.toEntity());
+        Article saved = service.save(dto.toEntity());
         log.info(saved.toString());
         return saved;
     }
 
     @GetMapping("/api/articles/{id}")
     public Article getArticle(@PathVariable("id") Long id) {
-        Article article = repository.findById(id).orElse(null);
-        return article;
+        return service.findById(id).orElse(null);
     }
 
     @GetMapping("/api/articles")
     public Page<Article> listArticles(Pageable pageable) {
-        Page<Article> all = repository.findAll(pageable);
-        return all;
+        return service.findAll(pageable);
     }
 
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> patch(@PathVariable("id") Long id, @RequestBody ArticleFormDTO dto) {
         log.info("id={}, request={}", id, dto.toString());
-        Article article = repository.findById(id).orElse(null);
-        if (article == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        article.patch(dto.toEntity());
-        log.info("updatedArticle={}", article);
-        repository.save(article);
+        Article article = service.patch(id, dto.toEntity());
         return ResponseEntity.ok(article);
     }
 
     @DeleteMapping("/api/articles/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
-        Article article = repository.findById(id).orElse(null);
-        if (article == null) {
-            return ResponseEntity.notFound().build();
-        }
-        repository.delete(article);
+        service.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
