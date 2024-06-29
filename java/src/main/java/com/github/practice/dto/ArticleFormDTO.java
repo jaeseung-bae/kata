4d802e5cedf9245afb9523c5beffdb3f0b3c64f5
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-06-29 13:19:43 +0900

    chore: lint code

--- java/src/main/java/com/github/practice/dto/ArticleFormDTO.java
+++ java/src/main/java/com/github/practice/dto/ArticleFormDTO.java
@@ -3,7 +3,6 @@ package com.github.practice.dto;
 import com.github.practice.domain.Article;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
