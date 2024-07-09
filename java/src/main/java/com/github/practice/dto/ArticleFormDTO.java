10a11bd6071c3ff3be397c83c0f458e23cca6163
jaeseung.bae <jaeseung.bae@linecorp.com>
2024-07-09 15:58:52 +0900

    Validate required fields

--- java/src/main/java/com/github/practice/dto/ArticleFormDTO.java
+++ java/src/main/java/com/github/practice/dto/ArticleFormDTO.java
@@ -3,6 +3,7 @@ package com.github.practice.dto;
 import com.github.practice.domain.Article;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,10 +17,15 @@ public class ArticleFormDTO {
     @Positive
     @Nullable
     private Long id;
+
+    @NotBlank(message = "it's required field")
     @Size(min = 1)
     private String title;
+
+    @NotBlank(message = "it's required field")
     @Size(min = 1)
     private String content;
+
     public Article toEntity() {
         return new Article(id, title, content);
     }
